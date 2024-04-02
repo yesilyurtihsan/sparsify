3d1c2b841f2a2f8c30ee617b903c83e0670ef2d0
yesilyurtihsan <yesilyrtihsan@gmail.com>
2024-04-02 18:46:23 +0300

    Comment out

--- src/crcs.cpp
+++ src/crcs.cpp
@@ -64,62 +64,62 @@ void ccs(int** A, int m, int n, int*& val, int*& row_ind, int*& col_ptr) {
     }
 }
 
-int main() {
-    srand(time(0));
-    const int m = 3;
-    const int n = 3;
-    int** A = new int*[m];
-    for (int i = 0; i < m; i++) {
-        A[i] = new int[n];
-    }
+// int main() {
+//     srand(time(0));
+//     const int m = 3;
+//     const int n = 3;
+//     int** A = new int*[m];
+//     for (int i = 0; i < m; i++) {
+//         A[i] = new int[n];
+//     }
 
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++){
-            int num = rand() % 100;
-            if (num < 70) A[i][j] = 0;
-            else A[i][j] = num;
-            }
-    // Initialize A with values
-    // Add this
+//     for (int i = 0; i < m; i++)
+//         for (int j = 0; j < n; j++){
+//             int num = rand() % 100;
+//             if (num < 70) A[i][j] = 0;
+//             else A[i][j] = num;
+//             }
+//     // Initialize A with values
+//     // Add this
 
 
-    int nnz = numNonZeros(A, m, n);
+//     int nnz = numNonZeros(A, m, n);
 
-    int* val_crs = new int[nnz];
-    int* col_ind_crs = new int[nnz];
-    int* row_ptr_crs;
-    crs(A, m, n, val_crs, col_ind_crs, row_ptr_crs);
+//     int* val_crs = new int[nnz];
+//     int* col_ind_crs = new int[nnz];
+//     int* row_ptr_crs;
+//     crs(A, m, n, val_crs, col_ind_crs, row_ptr_crs);
     
 
-    int* val_ccs = new int[nnz];
-    int* row_ind_ccs = new int[nnz];
-    int* col_ptr_ccs;
-    ccs(A, m, n, val_ccs, row_ind_ccs, col_ptr_ccs);
+//     int* val_ccs = new int[nnz];
+//     int* row_ind_ccs = new int[nnz];
+//     int* col_ptr_ccs;
+//     ccs(A, m, n, val_ccs, row_ind_ccs, col_ptr_ccs);
 
-    cout << nnz << endl;
+//     cout << nnz << endl;
 
-    // Use val_crs, col_ind_crs, row_ptr_crs, val_ccs, row_ind_ccs, col_ptr_ccs for further processing
+//     // Use val_crs, col_ind_crs, row_ptr_crs, val_ccs, row_ind_ccs, col_ptr_ccs for further processing
 
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++)
-           cout << A[i][j] << " ";
-        cout << endl;
-    }
+//     for (int i = 0; i < m; i++){
+//         for (int j = 0; j < n; j++)
+//            cout << A[i][j] << " ";
+//         cout << endl;
+//     }
 
-    cout << endl << nnz << endl << endl;
+//     cout << endl << nnz << endl << endl;
 
-    for (int i=0; i<nnz; i++)
-        cout << col_ind_crs[i] << " ";
+//     for (int i=0; i<nnz; i++)
+//         cout << col_ind_crs[i] << " ";
 
-    cout << endl << endl;
+//     cout << endl << endl;
 
-    for (int i=0; i<nnz; i++)
-        cout << val_crs[i] << " ";
+//     for (int i=0; i<nnz; i++)
+//         cout << val_crs[i] << " ";
 
-    cout << endl << endl;
+//     cout << endl << endl;
 
-    for(int i=0; i<m; i++)
-        cout << row_ptr_crs[i]<<endl;
+//     for(int i=0; i<m; i++)
+//         cout << row_ptr_crs[i]<<endl;
 
-    return 0;
-}
+//     return 0;
+// }
