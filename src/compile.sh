4299c0463a9dda2a9b128e576b4f0e94c6d6ac54
yesilyurtihsan <yesilyrtihsan@gmail.com>
2024-04-05 09:48:40 +0000

    Add compile script

---
+++ src/compile.sh
@@ -0,0 +1,11 @@
+g++ ryser.cpp -O0 -o r0
+g++ ryser.cpp -O1 -o r1
+g++ ryser.cpp -O2 -o r2
+g++ ryser.cpp -O3 -o r3
+g++ ryser.cpp -Ofast -o rf
+
+g++ ryserP.cpp -O0 -o p0
+g++ ryserP.cpp -O1 -o p1
+g++ ryserP.cpp -O2 -o p2
+g++ ryserP.cpp -O3 -o p3
+g++ ryserP.cpp -Ofast -o pf
